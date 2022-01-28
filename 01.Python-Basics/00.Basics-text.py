07b7d5bafa9a195b5d58a420c983f37d962621fb
Pramit Sahoo <pramitsahoo.gnipst@gmail.com>
2022-01-28 08:42:08 +0530

    Update 00.Basics-text.py

--- 01.Python-Basics/00.Basics-text.py
+++ 01.Python-Basics/00.Basics-text.py
@@ -1,2 +1,16 @@
 # this folder contains basics of python -
-- 
+"""
+01.Flow-Chart
+02.Introduction-to-Python
+03.Conditionals and loops
+04.Patterns 1
+05.Patterns 2
+06.More on loops
+07.Functions
+08.Arrays and Lists
+09.Searching and Sorting
+10.Strings
+11.Two Dimensional Lists
+12.Tuples Dictionary and Sets
+
+"""
